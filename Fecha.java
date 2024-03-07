37d0e4bbed3eaf759fef22a711e2bdfaf7aecaa1
unknown <jhojahnb@gmail.com>
2024-03-07 10:59:22 +0100

    version2

--- Fecha.java
+++ Fecha.java
@@ -2,13 +2,25 @@ package Transporte;
 import java.util.Calendar;
 import java.util.Date;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+@Data
+@Getter
+@Setter
 public class Fecha {
-  int a�o, mes, dia, hora, minuto;
+  int ano, mes, dia, hora, minuto;
+  
+  Fecha(int ano, int mes, int dia){
+	  this.ano=ano;
+	  this.mes=mes;
+	  this.dia=dia;
+  }
   Fecha() {
-		 Calendar calendario=Calendar.getInstance();  //Calendar: clase abstracta
+		 Calendar calendario=Calendar.getInstance();  
 		 Date fecha=calendario.getTime();
 		 calendario.setTime(fecha);
-		 a�o=calendario.get(Calendar.YEAR);
+		 ano=calendario.get(Calendar.YEAR);
 		 mes=calendario.get(Calendar.MONTH)+1;
 		 dia=calendario.get(Calendar.DAY_OF_MONTH);
 		 hora=calendario.get(Calendar.HOUR_OF_DAY);
@@ -21,22 +33,23 @@ public class Fecha {
   		int auxhPost= fPosterior.hora*60+fPosterior.minuto;
   		System.out.println(fPosterior.hora+":"+fPosterior.minuto+",,"
   		  +auxh+" ->"+auxhPost);
-  		if (a�o==fPosterior.a�o && mes==fPosterior.mes && 
+  		if (ano==fPosterior.ano && mes==fPosterior.mes && 
   				dia==fPosterior.dia && auxhPost<=auxh)
   			ok=true;
   		return ok;	
   	}
-	public Fecha(int a�o, int mes, int dia, int hora, int minuto) {
-	  this.a�o = a�o;
+  	
+	public Fecha(int ano, int mes, int dia, int hora, int minuto) {
+	  this.ano = ano;
 	  this.mes = mes;
 	  this.dia = dia;
 	  this.hora = hora;
 	  this.minuto = minuto;
-    }
+	  }
 
 	public static void main(String[] args) {
 		Fecha f= new Fecha();
-		System.out.println(f.a�o+":"+f.mes+":"+f.dia+":"+f.hora+":"+
+		System.out.println(f.ano+":"+f.mes+":"+f.dia+":"+f.hora+":"+
 		f.minuto);
 	}
 
